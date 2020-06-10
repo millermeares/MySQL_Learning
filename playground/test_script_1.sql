d55ca085a09cf8de5a344bbaab410ed9cf5d9968
Ben Meares <benmillermeares3@gmail.com>
2020-06-10 17:59:11 -0400

    adding data at the start of section 7 on String manipulation

--- playground/test_script_1.sql
+++ playground/test_script_1.sql
@@ -1,8 +1,16 @@
+CREATE DATABASE cats_app;
+USE cats_app;
 CREATE TABLE cats
 	(
 		cat_id INT NOT NULL AUTO_INCREMENT,
         name VARCHAR(100),
         age INT,
-        PRIMARY KEY (cat_id)
+        PRIMARY KEY(cat_id)
 	);
-        
+    
+INSERT INTO cats(name, AGE)
+VALUES
+('Charlie', 17),
+('Connie', 10);
+
+SELECT * FROM cats;
